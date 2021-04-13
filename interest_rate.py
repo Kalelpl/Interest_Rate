04c8226b94766bc361ec827250234f3717ef5f78
jakub_masniak <jakub.masniak@huuugegames.com>
2021-04-13 11:29:50 +0200

    Fixed some typo mistakes

--- interest_rate.py
+++ interest_rate.py
@@ -1,8 +1,8 @@
 # Get the load details
-money_owed = float(input("How much money do you owe, in dollars?\n")) # $50,000
-apr = float(input("What is the annual percentage rate?\n")) # 3%
-payment = float(input("What will your monthly payment be, in dollars?\n")) # $1.000
-months = int(input("How many months do you want to see results for?\n")) # 24
+money_owed = float(input("How much money do you owe, in dollars?\n"))  # $50,000
+apr = float(input("What is the annual percentage rate?\n"))  # 3%
+payment = float(input("What will your monthly payment be, in dollars?\n"))  # $1.000
+months = int(input("How many months do you want to see results for?\n"))  # 24
 
 # Divide apr by 100 to make it a percent,u then divide by 12 to make monthly
 monthly_rate = apr/100/12
